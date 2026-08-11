b44130a6296194ff6ad2df417b2a36248b44ff00
nithinsshetty <nithinsshetty3@gmail.com>
2026-08-11 10:24:46 +0530

    Clean up legacy Firebase files and project artifacts

--- print_db_schema_rest.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = 'C:\\Users\\NITHIN S SHETTY\\.gemini\\antigravity\\brain\\070bd493-edb6-4f37-a724-6539b2cc9214\\database_schema_document.md';
-
-if (fs.existsSync(file)) {
-  const content = fs.readFileSync(file, 'utf8');
-  console.log(content.substring(5000));
-} else {
-  console.log('File does not exist.');
-}
